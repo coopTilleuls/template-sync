d4b73b452c0cb7430b028a628b58a05ac9f4034d
Manu <emmanuel.barat@les-tilleuls.coop>
2023-09-27 10:42:45 +0200

    beginning of the script

---
+++ franken.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+initial_commit=$(git rev-list "$(git rev-parse HEAD)" --max-parents==0)
+
+echo $initial_commit 
+
+current_dir=$(pwd)
+
+temp_dir=$(mktemp -d)
+
+git clone https://github.com/api-platform/api-platform "$temp_dir"
+
+cd "$temp_dir" || return
+
+api_shas=$(git log --pretty=format:"%H")
+
+for sha in $api_shas;
+do
+    diff=$(git diff "$initial_commit" "$sha")
+    if [ -z "$diff" ]; then
+    initial_api_commit=$sha
+    break
+    fi
+done
+
+echo "$initial_api_commit"
+ 
